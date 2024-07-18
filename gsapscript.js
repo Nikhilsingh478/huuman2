e69d40340a230db67b443fea19adcdf46588669c
Nikhil Singh <neelsingh45940s@gmail.com>
2024-07-18 21:35:58 +0530

    huuman_official effect

--- gsapscript.js
+++ gsapscript.js
@@ -55,3 +55,17 @@ gsap.from("#imagediv", {
     duration: 2.5,
     ease: "power4.out"
 });
+
+
+gsap.to("#page2 h1",{
+    transform:"TranslateX(-270%)",
+    scrollTrigger:{
+       trigger:"#page2",
+       scroller:"body",
+    //    markers:true,
+       start:"top 0%",
+       end:"top -100%",
+       scrub:5,
+       pin:true
+    }
+})
