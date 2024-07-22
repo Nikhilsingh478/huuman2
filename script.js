76f84dbe95fdc1efea877d3342daafe9d15dbb8a
Nikhil Singh <neelsingh45940s@gmail.com>
2024-07-22 21:55:23 +0530

    shery.js gooey effect minor fixes and products section hover effect fix 22 jul 10 pm

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-Shery.imageEffect("#back", {style: 5, config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.91,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.2224052718286655},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":5,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},gooey: true })
+Shery.imageEffect("#back" , { style:5,config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.4634994206257241},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0.25,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4.53,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.7,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.18,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.53,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":1725}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}} ,gooey:true })
+
+
 
 var elems = document.querySelectorAll(".elem");
 elems.forEach(function(elem){
@@ -40,37 +42,7 @@ setTimeout(() => {
 
 
 
-// $(document).ready(function(){
-//     $('.testimonials-grid').slick({
-//      arrows:false,
-//      dots:true,
-//      appendDots:'.slider-dots',
-//      dotsClass:'dots'
-//     });
-// });
-
-
-// script for cursor 
-// var body = document.querySelector("body");
-// var cursor = document.querySelector("#cursor");
-
-// body.addEventListener("mousemove",function(param){
-//     gsap.to(cursor,{
-//       x:param.x,
-//       y:param.y,
-//       duration:0.7
-//     })    
-// })
-
-
-
-
-
-
-
-
-
-
+// script for cursor starts 
 var body = document.querySelector("body");
 var cursor = document.querySelector("#cursor");
 var bubblesContainer = document.querySelector(".bubbles");
